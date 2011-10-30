24c2e068544ef452eb5af4c3ed6a72404f4e2d1c
Ramanathan Balakrishnan <ramanathan@ramanathan-Latitude-E6500.(none)>
2011-10-30 22:23:27 +0530

    added UI status messages

--- src/com/thoughtworks/android/FacebookBirthdayCalendar.java
+++ src/com/thoughtworks/android/FacebookBirthdayCalendar.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.android;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,19 +15,21 @@ import com.thoughtworks.android.listener.FriendsRequestListener;
 import com.thoughtworks.android.model.Friends;
 
 public class FacebookBirthdayCalendar extends Activity implements FacebookListener {
-    private Facebook facebook;
-    private Calendar calendar;
     private View view;
+    private Facebook facebook;
+    private ProgressDialog progressDialog;
     private static final String APP_ID = "216175911783054";
 
     public FacebookBirthdayCalendar() {
-        facebook = new Facebook(APP_ID);
         view = new View();
+        facebook = new Facebook(APP_ID);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        progressDialog = new ProgressDialog(this);
+        showStatus("Authorizing...");
         facebook.authorize(this, new String[]{"friends_birthday"}, new AuthorizationDialogListener(this));
     }
 
@@ -36,35 +39,54 @@ public class FacebookBirthdayCalendar extends Activity implements FacebookListen
         facebook.authorizeCallback(requestCode, resultCode, data);
     }
 
+    public void notifyAuthorizationSuccess() {
+        showStatusOnUiThread("Fetching friends...");
+        fetchFriends();
+    }
+
+    public void notifyFriendsRecieved(Friends friends) {
+        showStatusOnUiThread("Adding friends to calendar...");
+        new Calendar(this).addBirthdays(friends);
+        showBirthday(friends);
+    }
+
+    private void fetchFriends() {
+        AsyncFacebookRunner asyncFacebookRunner = new AsyncFacebookRunner(facebook);
+        Bundle friendFields = new Bundle();
+        friendFields.putString("fields", "birthday,name");
+        asyncFacebookRunner.request("me/friends", friendFields, new FriendsRequestListener(this));
+    }
+
     private void showBirthday(final Friends friends) {
         runOnUiThread(new Runnable() {
             public void run() {
+                hideStatus();
                 Context applicationContext = getApplicationContext();
                 ScrollView scrollView = new ScrollView(applicationContext);
                 TextView textView = new TextView(applicationContext);
-                textView.setText(view.getFriendsHavingBirthday(friends));
+                String status = "The following friends have been added to your calendar:\n\n";
+                textView.setText(status + view.getFriendsHavingBirthday(friends));
                 scrollView.addView(textView);
                 setContentView(scrollView);
             }
         });
     }
 
-    @Override
-    public void notifyFriendsRecieved(Friends friends) {
-        calendar = new Calendar(this);
-        calendar.addBirthdays(friends);
-        showBirthday(friends);
+    private void showStatusOnUiThread(final String message) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                hideStatus();
+                showStatus(message);
+            }
+        });
     }
 
-    @Override
-    public void notifyAuthorizationSuccess() {
-        fetchMyFriends();
+    private void showStatus(String message) {
+        progressDialog.setMessage(message);
+        progressDialog.show();
     }
 
-    private void fetchMyFriends() {
-        AsyncFacebookRunner asyncFacebookRunner = new AsyncFacebookRunner(facebook);
-        Bundle friendFields = new Bundle();
-        friendFields.putString("fields", "birthday,name");
-        asyncFacebookRunner.request("me/friends", friendFields, new FriendsRequestListener(this));
+    private void hideStatus() {
+        progressDialog.hide();
     }
 }
