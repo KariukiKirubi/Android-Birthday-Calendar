4bf1cfb05d366148b5f54006dd56e1c2768f23e7
Ramanathan Balakrishnan <ramanathan@ramanathan-Latitude-E6500.(none)>
2011-11-17 22:13:16 +0530

    fixed timezone offset

--- src/com/thoughtworks/android/model/Birthday.java
+++ src/com/thoughtworks/android/model/Birthday.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.android.model;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class Birthday {
@@ -17,8 +18,11 @@ public class Birthday {
         String[] dateFields = date.split("/");
         int day = Integer.parseInt(dateFields[1]);
         int month = Integer.parseInt(dateFields[0]) - 1;
-        int year = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR) - 1900;
-        return new Date(year, month, day).getTime();
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(java.util.Calendar.YEAR) - 1900;
+        int timezoneOffset = calendar.get(Calendar.ZONE_OFFSET)
+                + calendar.get(Calendar.DST_OFFSET);
+        return new Date(year, month, day).getTime() + timezoneOffset;
     }
 
     @Override
